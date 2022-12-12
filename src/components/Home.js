a8ebe4857a0fd541260639f03989a6edd5d8a369
gobikris <gobikrishna005@gmail.com>
2022-12-12 17:26:48 +0530

    loader & formating file

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,77 +1,200 @@
-
-
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div>
-        <nav class="navbar navbar-expand-lg bg-primary fixed-top">
-  <div class="container-fluid ">
-    <img className='img-fluid me-5' style={{width:"120px"}} src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/fk-plus_3b0baa.png" alt="" />
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse " id="navbarSupportedContent">
-    <input type="text" className='form-control w-50' placeholder='search for product,brand and more' name="" id="" />
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex gap-4">
+      <nav class="navbar navbar-expand-lg bg-primary fixed-top">
+        <div class="container-fluid ">
+          <img
+            className="img-fluid me-5"
+            style={{ width: "120px" }}
+            src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/fk-plus_3b0baa.png"
+            alt=""
+          />
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <input
+              type="text"
+              className="form-control w-50"
+              placeholder="search for product,brand and more"
+              name=""
+              id=""
+            />
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex gap-4">
+              <li class="nav-item"></li>
 
-        <li class="nav-item">
-          
-        </li>
-        
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle active text-light fw-bold" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            User
-          </a>
-          <ul class="dropdown-menu">
-          <li><Link class="dropdown-item" to="#"><i class="fa fa-user text-primary" aria-hidden="true"></i> My Profile</Link></li>
-             <li><hr class="dropdown-divider"/></li>
-             <li><Link class="dropdown-item" to="#"><i class="fa fa-plus-circle text-primary" aria-hidden="true"></i> SuperCoin Zone</Link></li>
-             <li><hr class="dropdown-divider"/></li>
-            <li><Link class="dropdown-item" to="#"><i class="fa fa-archive text-primary" aria-hidden="true"></i> Orders</Link></li>
-            <li><hr class="dropdown-divider"/></li>
-             <li><Link class="dropdown-item" to="#"><i class="fa fa-heart text-primary" aria-hidden="true"></i> Wishlist</Link></li>
-             <li><hr class="dropdown-divider"/></li>
-             <li><Link class="dropdown-item" to="#"><i class="fa fa-ticket text-primary" aria-hidden="true"></i> Coupons</Link></li>
-             <li><hr class="dropdown-divider"/></li>
-             <li><Link class="dropdown-item" to="#"><i className="fa fa-gift text-primary" aria-hidden="true"></i> Gift Cards</Link></li>
-             <li><hr class="dropdown-divider"/></li>
-             <li><Link class="dropdown-item" to="#"><i class="fa fa-bell text-primary" aria-hidden="true"></i> Notification</Link></li>
-          </ul>
-        </li>
-        <li class="nav-item ">
-          <a class="nav-link active text-light fw-bold" href="#">Become a Seller</a>
-        </li>
+              <li class="nav-item dropdown ">
+                <a
+                  class="nav-link dropdown-toggle active text-light fw-bold"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  User
+                </a>
+                <ul class="dropdown-menu ">
+                  <li>
+                    <Link class="dropdown-item" to="#">
+                      <i class="fa fa-user text-primary" aria-hidden="true"></i>{" "}
+                      My Profile
+                    </Link>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <Link class="dropdown-item" to="#">
+                      <i
+                        class="fa fa-plus-circle text-primary"
+                        aria-hidden="true"
+                      ></i>{" "}
+                      SuperCoin Zone
+                    </Link>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <Link class="dropdown-item" to="#">
+                      <i
+                        class="fa fa-archive text-primary"
+                        aria-hidden="true"
+                      ></i>{" "}
+                      Orders
+                    </Link>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <Link class="dropdown-item" to="#">
+                      <i
+                        class="fa fa-heart text-primary"
+                        aria-hidden="true"
+                      ></i>{" "}
+                      Wishlist
+                    </Link>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <Link class="dropdown-item" to="#">
+                      <i
+                        class="fa fa-ticket text-primary"
+                        aria-hidden="true"
+                      ></i>{" "}
+                      Coupons
+                    </Link>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <Link class="dropdown-item" to="#">
+                      <i
+                        className="fa fa-gift text-primary"
+                        aria-hidden="true"
+                      ></i>{" "}
+                      Gift Cards
+                    </Link>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <Link class="dropdown-item" to="#">
+                      <i class="fa fa-bell text-primary" aria-hidden="true"></i>{" "}
+                      Notification
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item ">
+                <a class="nav-link active text-light fw-bold" href="#">
+                  Become a Seller
+                </a>
+              </li>
 
-        <li class="nav-item dropdown ">
-          <a class="nav-link dropdown-toggle active text-light fw-bold" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            More
-          </a>
-          <ul class="dropdown-menu ">
-          <li><Link class="dropdown-item" to="#"><i class="fa fa-bell text-primary" aria-hidden="true"></i> Notification Preference</Link></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><Link class="dropdown-item" to="#"><i class="fa fa-question-circle text-primary" aria-hidden="true"></i> 24 X 7 Customer Care</Link></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><Link class="dropdown-item" to="#"><i class="fa fa-line-chart text-primary" aria-hidden="true"></i> Advertise</Link></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><Link class="dropdown-item" to="#"><i className="fa fa fa-download text-primary" aria-hidden="true"></i> Download App</Link></li>
-          </ul>
-        </li>
+              <li class="nav-item dropdown ">
+                <a
+                  class="nav-link dropdown-toggle active text-light fw-bold"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  More
+                </a>
+                <ul class="dropdown-menu ">
+                  <li>
+                    <Link class="dropdown-item" to="#">
+                      <i class="fa fa-bell text-primary" aria-hidden="true"></i>{" "}
+                      Notification Preference
+                    </Link>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <Link class="dropdown-item" to="#">
+                      <i
+                        class="fa fa-question-circle text-primary"
+                        aria-hidden="true"
+                      ></i>{" "}
+                      24 X 7 Customer Care
+                    </Link>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <Link class="dropdown-item" to="#">
+                      <i
+                        class="fa fa-line-chart text-primary"
+                        aria-hidden="true"
+                      ></i>{" "}
+                      Advertise
+                    </Link>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <Link class="dropdown-item" to="#">
+                      <i
+                        className="fa fa fa-download text-primary"
+                        aria-hidden="true"
+                      ></i>{" "}
+                      Download App
+                    </Link>
+                  </li>
+                </ul>
+              </li>
 
-        
-        <li class="nav-item">
-          <a class="nav-link active fw-bold text-light"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart</a>
-        </li>
-      </ul>
-      
+              <li class="nav-item">
+                <a class="nav-link active fw-bold text-light">
+                  <i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav>
-
-
-    </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,84 +1,130 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import axios from 'axios';
-import { useEffect } from 'react';
+import axios from "axios";
+import { useEffect } from "react";
 
 function Product() {
+  // navigate
+  const navigate = useNavigate();
 
-  
+  // data
+  const [list, setList] = useState({});
 
-const navigate = useNavigate()
+  // loader
+  const [isLoading, setIsLoading] = useState(true);
 
-const [list,setList] = useState({})
-
-
-
-
-  const getAll = async () =>{
+  // Api
+  const getAll = async () => {
     try {
-      const {data} = await axios.get(`https://62556e3352d8738c6921d09a.mockapi.io/user`)
-      console.log(data)
-      setList(data)
+      const { data } = await axios.get(
+        `https://62556e3352d8738c6921d09a.mockapi.io/user`
+      );
+      console.log(data);
+      setList(data);
+      setIsLoading(false);
+
     } catch (error) {
       console.log(error.message);
     }
-      
-  }
+  };
 
-useEffect(()=>{
-getAll();
-},[])
-
-
+  useEffect(() => {
+    getAll();
+  }, []);
 
   return (
     <>
-    
-      <div className="container" style={{marginTop:"120px"}}>
+      <div className="container" style={{ marginTop: "120px" }}>
         <div className="row mb-2">
-          {list.map && list.filter((s)=>s.name.toLowerCase()).map((g,i)=>{
-            const {name,id,price,mrp,miniName,offer,img} = g
-            return(
-              <>
-              <div className="col mb-3">
 
-              <div class="card hands shadow-lg mx-auto" style={{width: "18rem"}} key={i} onClick={()=>navigate(`/info/${id}`)}>
-  <img src={img} class="card-img-top pro" alt="..."/>
-  <div class="card-body">
-    <div className='d-flex  justify-content-between'>
-    <h6 className='text-secondary'>{miniName} <img style={{width:"4rem"}} src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/fa_62673a.png" alt="" /></h6>
-    
-    </div>
-    
-    <h5 class="card-title">{name} </h5>
-    <p class="card-text"></p>
-    <div className='d-flex gap-3'>
-    <p class="fw-bold">₹ {price}</p>
-    <s className='fw-bold text-secondary'>₹{mrp}</s>
-    <small className='colour2 fw-bold'>{offer} Off %</small>
-    
-    </div>
-    <div className='d-flex justify-content-between'>
-    <small>Free Delivery</small>
-    <i class="fa fa-heart text-secondary" aria-hidden="true"></i>
-    </div>
-    
-  </div>
-</div>
+        {isLoading && (
+          <div className="text-center mt-5">
+
+            {/* loader */}
+            <div className="spinner-grow text-primary" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow text-secondary" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow text-success" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow text-danger" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow text-warning" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow text-info" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow text-danger" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow text-dark" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            
+          </div>
+        )}
 
 
-              </div>
-              </>
-            )
-          })}
+            {/* data */}
+          {list.map &&
+            list
+              .filter((s) => s.name.toLowerCase())
+              .map((g, i) => {
+                const { name, id, price, mrp, miniName, offer, img } = g;
+                return (
+                  <>
+                    <div className="col mb-3">
+                      <div
+                        class="card hands shadow-lg mx-auto"
+                        style={{ width: "18rem" }}
+                        key={i}
+                        onClick={() => navigate(`/info/${id}`)}
+                      >
+                        <img src={img} class="card-img-top pro" alt="..." />
+                        <div class="card-body">
+                          <div className="d-flex  justify-content-between">
+                            <h6 className="text-secondary">
+                              {miniName}{" "}
+                              <img
+                                style={{ width: "4rem" }}
+                                src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/fa_62673a.png"
+                                alt=""
+                              />
+                            </h6>
+                          </div>
+
+                          <h5 class="card-title">{name} </h5>
+                          <p class="card-text"></p>
+                          <div className="d-flex gap-3">
+                            <p class="fw-bold">₹ {price}</p>
+                            <s className="fw-bold text-secondary">₹{mrp}</s>
+                            <small className="colour2 fw-bold">
+                              {offer} Off %
+                            </small>
+                          </div>
+                          <div className="d-flex justify-content-between">
+                            <small>Free Delivery</small>
+                            <i
+                              class="fa fa-heart text-secondary"
+                              aria-hidden="true"
+                            ></i>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
         </div>
       </div>
-
-
-
     </>
-  )
+  );
 }
 
-export default Product
+export default Product;
